16b4f17e4f5da74bf19f5a1293664754243ca355
Yagnikfadadu <100338428+Yagnikfadadu@users.noreply.github.com>
2022-04-19 13:47:55 +0530

    Update test.java

--- test.java
+++ test.java
@@ -7,20 +7,50 @@ public class test
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter text :");
         String userInput = sc.nextLine();
-        String encryptedText="";
-        ArrayList<String> l1 = PerformEncryption.Iteration(userInput);
-        for (String s: l1)
+        String encryption1= "";
+        ArrayList<String> iteration1 = PerformEncryption.Iteration(userInput);
+        for (String s: iteration1)
         {
-            encryptedText += s;
+            encryption1 += s;
         }
-        System.out.println(encryptedText);
+        System.out.println(encryption1);
 
-        ArrayList<String> l2 = PerformDecryption.Iteration(encryptedText);
-        String decryptedText="";
-        for (String s: l2)
+        ArrayList<String> iteration2 = PerformDoubleEncryption.Encrypt(encryption1);
+        String encryption2="";
+        for (String s: iteration2)
         {
-            decryptedText += s;
+            encryption2 += s;
         }
-        System.out.println(decryptedText);
+        System.out.println(encryption2);
+
+        String encryption3 = "";
+        ArrayList<String> iteration3 = PerformMorse.getMorse(encryption2);
+        for (String s: iteration3)
+        {
+            encryption3 +=s;
+        }
+
+        String decryption1 = "";
+        ArrayList<String> iteration4 = PerformMorse.getString(iteration3);
+        for (String s: iteration3)
+        {
+            decryption1 +=s;
+        }
+        System.out.println(iteration4);
+
+        String decryption2 = "";
+        ArrayList<String> iteration5 = PerformDoubleDecryption.Decrypt(decryption1);
+        for (String s:iteration5)
+        {
+            decryption2 +=s;
+        }
+
+        String decryption3 = "";
+        ArrayList<String> iteration6 = PerformDecryption.Iteration(decryption2);
+        for (String s:iteration6)
+        {
+            decryption3 +=s;
+        }
+        System.out.println(decryption2);
     }
 }
